2961cba9698dbda1b81d5aa452d1a1edc33355e3
sadurtinova <sadurtinova@speechpro.com>
2018-08-21 15:07:59 +0300

    Python SpeechCloud client
    Python modules for STC SpeechCloud generated by swagger codegen

---
+++ tts/cloud_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cloud_client.api.session_api import SessionApi
+from cloud_client.api.synthesize_api import SynthesizeApi
