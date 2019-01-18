c0d551a8b202e162fab746ca1d2bbc82e051894e
sadurtinova <sadurtinova@speechpro.com>
2019-01-18 17:22:43 +0300

    Update for 1.1

--- asr/cloud_client/__init__.py
+++ asr/cloud_client/__init__.py
@@ -26,13 +26,12 @@ from cloud_client.configuration import Configuration
 # import models into sdk package
 from cloud_client.models.asr_result_dto import ASRResultDto
 from cloud_client.models.audio_file_dto import AudioFileDto
-from cloud_client.models.exception_dto import ExceptionDto
 from cloud_client.models.package_dto import PackageDto
 from cloud_client.models.recognition_request_dto import RecognitionRequestDto
-from cloud_client.models.session_dto import SessionDto
 from cloud_client.models.sessionless_recognition_request_dto import SessionlessRecognitionRequestDto
 from cloud_client.models.start_session_request import StartSessionRequest
 from cloud_client.models.status_dto import StatusDto
 from cloud_client.models.stream_request_dto import StreamRequestDto
-from cloud_client.models.web_socket_server_configuration import WebSocketServerConfiguration
 from cloud_client.models.word_dto import WordDto
+from cloud_client.models.auth_response_dto import AuthResponseDto
+from cloud_client.models.auth_status_dto import AuthStatusDto
