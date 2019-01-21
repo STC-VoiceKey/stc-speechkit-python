17691b656c204fff6c61a2bbfa4d600d155e2b94
sadurtinova <sadurtinova@speechpro.com>
2019-01-21 12:24:23 +0300

    Update for version 1.1

--- tts/cloud_client/api/__init__.py
+++ tts/cloud_client/api/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from cloud_client.api.session_api import SessionApi
-from cloud_client.api.synthesize_api import SynthesizeApi
+import cloud_client.api.session_api
+import cloud_client.api.synthesize_api
